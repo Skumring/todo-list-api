37beb2f899b9ce7dfbbd4bc10d7019a469576c21
Denys Metelov <alatelb@gmail.com>
2020-04-02 09:42:59 +0300

    Added welcome page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,31 +2,31 @@ class TodosController < ApplicationController
   before_action :set_todo, only: [:update, :destroy]
   
   def index
-     todos = Todo.order(created_at: :desc)
-     render json: todos
-   end
-   
-   def create
-     todo = Todo.create(todo_param)
-     render json: todo
-   end
-   
-   def update
-     @todo.update_attributes(todo_param)
-     render json: @todo
-   end
-   
-   def destroy
-     @todo.destroy
-     head :no_content, status: :ok
-   end
-   
-   private
-     def set_todo
-       @todo = Todo.find(params[:id])
-     end
-     
-     def todo_param
-       params.require(:todo).permit(:title, :done)
-     end
+    todos = Todo.order(created_at: :desc)
+    render json: todos
+  end
+  
+  def create
+    todo = Todo.create(todo_param)
+    render json: todo
+  end
+  
+  def update
+    @todo.update_attributes(todo_param)
+    render json: @todo
+  end
+  
+  def destroy
+    @todo.destroy
+    head :no_content, status: :ok
+  end
+  
+  private
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+    
+    def todo_param
+      params.require(:todo).permit(:title, :done)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  
   scope '/api/v1' do
     resources :todos
   end
