1e1f322fda5e30b9b0cc202f1875ccfada778df0
Denys Metelov <alatelb@gmail.com>
2020-12-11 11:57:50 +0200

    Cleaning the seeds

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-Todo.create(title: 'Buy food: milk, bread, fruits', done: false)
-Todo.create(title: 'Process the specification for fill the estimation', done: false)
-Todo.create(title: 'Finish feature "Contract Service"', done: false)
